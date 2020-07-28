a05051ca767607568f07043c1370aa9fd0a32ca5
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-28 10:34:38 +0200

    Modularize Schemas / Resolvers and apply good practices

--- templates/graphql-hello-world/src/schema/index.ts
+++ templates/graphql-hello-world/src/schema/index.ts
@@ -1,8 +1,9 @@
 import { GraphQLSchema } from 'graphql';
-import 'graphql-import-node';
-import typeDefs from './schema.graphql';
-import resolvers from './../resolvers/resolversMap';
+import resolvers from '../resolvers';
 import { makeExecutableSchema } from 'graphql-tools';
+import { loadFilesSync }  from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+const typeDefs = mergeTypeDefs(loadFilesSync(`${__dirname}/**/*.graphql`));
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
